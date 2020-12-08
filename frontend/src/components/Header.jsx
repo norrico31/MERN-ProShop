30657ac9edf5c9536639ebf46a15e7b6ee3750cf
norrico31 <geraldbiasonjob31.com>
2020-12-08 22:32:50 +0800

    Rating component to HomeScreen.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
     return (
         <header>
-            <Navbar bg="dark" variant="dark" expand="lg" CollapseOnSelect>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <Navbar.Brand href="/">ProShop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,19 +1,17 @@
 import { Row, Col } from 'react-bootstrap'
 import products from '../products'
-import Product from './Product'
+import Product from '../components/Product'
 
 const HomeScreen = () => {
     return (
         <>
             <h1>Latest Products</h1>
             <Row>
-                {products.map(product => {
-                    return (
-                        <Col sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    )
-                })}
+                {products.map(product => (
+                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                        <Product product={product} />
+                    </Col>
+                ))}
             </Row>
         </>
     )
