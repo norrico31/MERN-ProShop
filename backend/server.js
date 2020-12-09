468c48c31e77aebf9683881961e3316dde20d99c
norrico31 <geraldbiasonjob31.com>
2020-12-09 21:06:29 +0800

    create a custom middleware for errorHandling and notFound

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import db from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 const app = express()
 
 dotenv.config()
@@ -14,6 +16,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
