1b1e940c06e2be0f876a8e6e7c299dfc11a07954
norrico31 <geraldbiasonjob31.com>
2020-12-10 12:14:38 +0800

    added constants variable for types in reducers and actions. created a productList reducer function and listProducts action creator

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,18 @@
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants'
+import axios from 'axios'
+
+export const listProducts = () => async dispatch => {
+    try {
+        dispatch({ type: PRODUCT_LIST_REQUEST })
+        const res = await axios.get('/api/products')
+        dispatch({ 
+            type: PRODUCT_LIST_SUCCESS,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({ 
+            type: PRODUCT_LIST_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
