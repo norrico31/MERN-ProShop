0746def4e7bdd18537bb71e3cfca656be8d167b5
norrico31 <geraldbiasonjob31.com>
2020-12-12 20:10:57 +0800

    created a userController.js and userRoutes.js for user authentication/login. creating a prototype methods in userModel/database matchPassword() to compare the plain text password from req.body and hash password from this.password

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,21 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import db from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const app = express()
 
 dotenv.config()
-
 db()
 
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
