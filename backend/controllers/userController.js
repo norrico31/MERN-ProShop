8113c441cf990cc5ddca6f1055e554be4e03eaed
norrico31 <geraldbiasonjob31.com>
2020-12-12 20:47:19 +0800

    jsonwebtoken installed and generateToken created in the utils folder to create a token for user id in the userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import AsyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import { generateToken } from '../utils/generateToken.js'
 
 // @desc     Auth user & get token
 // @route    POST /api/users/login
@@ -13,7 +14,7 @@ const authUser = AsyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmine: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
