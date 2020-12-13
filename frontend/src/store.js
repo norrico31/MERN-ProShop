61881df7f0943308b94858727930eb563561ed13
norrico31 <geraldbiasonjob31.com>
2020-12-13 14:36:53 +0800

    userReducers and userActions created to provide the login and save the info to localStorage on the state of userLogin: { userInfo }

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,16 +3,21 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducers } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers.js'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducers
+    cart: cartReducers,
+    userLogin: userLoginReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 const middleware = [thunk]
 
