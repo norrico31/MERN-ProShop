ba2756d08bc425623bdd25dd53555fc4b5df3922
norrico31 <geraldbiasonjob31.com>
2020-12-13 15:47:43 +0800

    customize the Header if the userInfo has a token in the localStorage and logout functionality added in action creator and used in Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 
 const Header = () => {
+    const { userInfo } = useSelector(state => state.userLogin)
+    const dispatch = useDispatch()
+
+    const onLogoutHandler = () => {
+        dispatch(logout())
+    }
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -15,9 +23,19 @@ const Header = () => {
                             <LinkContainer to="/cart">
                                 <Nav.Link><i className="fas fa-shopping-cart"/> Cart</Nav.Link>
                             </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={onLogoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
                             <LinkContainer to="/login">
                                 <Nav.Link><i className="fas fa-user"/> Sign In</Nav.Link>
                             </LinkContainer>
+                        )}
+                            
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
