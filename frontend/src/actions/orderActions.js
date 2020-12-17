b2aa45f7d13e0c15f9122cc41505d9bfd6dc7e05
norrico31 <geraldbiasonjob31.com>
2020-12-17 22:17:44 +0800

    created orderActions orderConstants and orderReducer for getOrderDetails in OrderScreen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from '../constants/orderConstants'
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from '../constants/orderConstants'
 import axios from 'axios'
 
 export const createOrder = order => async (dispatch, getState) => {
@@ -21,6 +21,30 @@ export const createOrder = order => async (dispatch, getState) => {
     } catch (err) {
         dispatch({ 
             type: ORDER_CREATE_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+export const getOrderDetails = orderId => async (dispatch, getState) => {
+    const { userLogin: { userInfo }} = getState()
+    try {
+        dispatch({
+            type: ORDER_DETAILS_REQUEST
+        })
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data } = await axios.get(`/api/orders/${orderId}`, config)
+        dispatch({
+            type: ORDER_DETAILS_SUCCESS,
+            payload: data 
+        })
+    } catch (err) {
+        dispatch({ 
+            type: ORDER_DETAILS_FAIL,
             payload: err.response && err.response.data.message ? err.response.data.message : err.message
         })
     }
