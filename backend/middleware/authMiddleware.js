89b8c9c1fafa7a8ab89e205003b48bcbeb1a5052
norrico31 <geraldbiasonjob31.com>
2020-12-19 16:41:02 +0800

    change the authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,22 +2,22 @@ import AsyncHandler from 'express-async-handler'
 import jwt from 'jsonwebtoken'
 import User from '../models/userModel.js'
 
-export const protect = AsyncHandler(async (req, res, next) => {
+export const protect = async (req, res, next) => {
     let token
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            console.log(decoded)
             req.user = await User.findById(decoded.id).select('-password')
             next()
         } catch (error) {
-            console.error(error)
-            res.status(401)
-            throw new Error('Not authorized, token failed')
+            res.status(401).json({ msg: 'Not authorized, token failed' })
+            // throw new Error('Not authorized, token failed')
         }
     }
     if (!token) {
-        res.status(401)
-        throw new Error('Not authorized, no token')
+        res.status(401).json({ msg: 'Not authorized, no token' })
+        // throw new Error('Not authorized, no token')
     }
-})
+}
