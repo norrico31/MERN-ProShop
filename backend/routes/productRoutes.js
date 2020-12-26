95520b583f82d0368e84e5e0d9fd590d5436fbce
norrico31 <norricobiason31@gmail.com>
2020-12-26 22:23:45 +0800

    backend all set for create and update a product by admin only.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts, getProductById, deleteProductByAdmin } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProductByAdmin, updateProductByAdmin, createProductByAdmin } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProductByAdmin)
+router.route('/').get(getProducts).post(protect, admin, createProductByAdmin)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProductByAdmin).put(protect, admin, updateProductByAdmin)
 
 export default router
