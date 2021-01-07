eafb6d8edee957dc7683c67b241bdd2ca60edb10
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-01-07 10:19:08 +0800

    added an endpoint for product review in productController backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts, getProductById, deleteProductByAdmin, updateProductByAdmin, createProductByAdmin } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProductByAdmin, updateProductByAdmin, createProductByAdmin, createProductReview } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProductByAdmin)
+router.route('/:id/reviews').post(protect, createProductReview)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProductByAdmin).put(protect, admin, updateProductByAdmin)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import db from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -16,6 +17,10 @@ dotenv.config()
 db()
 
 app.use(express.json())
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
